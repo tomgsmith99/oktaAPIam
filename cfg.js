6f5526b250218674fc902c289b8e9d603bc4272e
Tom Smith <tom.smith@okta.com>
2018-01-28 16:52:08 -0500

    tweaked env

--- cfg.js
+++ cfg.js
@@ -2,24 +2,19 @@
 global._CFG = {}
 
 global._CFG.APIGEE = {}
-global._CFG.APIGEE.CLIENT_ID = process.env.APIGEE_CLIENT_ID
-global._CFG.APIGEE.CLIENT_SECRET = process.env.APIGEE_CLIENT_SECRET
 global._CFG.APIGEE.PROXY_URI = process.env.APIGEE_PROXY_URI
 
 global._CFG.GOOGLE = {}
-global._CFG.GOOGLE.CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-global._CFG.GOOGLE.CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 global._CFG.GOOGLE.PROXY_URI = process.env.GOOGLE_PROXY_URI
 
 global._CFG.MULESOFT = {}
-global._CFG.MULESOFT.CLIENT_ID = process.env.MULESOFT_CLIENT_ID
-global._CFG.MULESOFT.CLIENT_SECRET = process.env.MULESOFT_CLIENT_SECRET
 global._CFG.MULESOFT.PROXY_URI = process.env.MULESOFT_PROXY_URI
 
 global.OKTA_CLIENT_ID = process.env.OKTA_CLIENT_ID
 global.OKTA_CLIENT_SECRET = process.env.OKTA_CLIENT_SECRET
 global.OKTA_AUTH_SERVER_ID = process.env.OKTA_AUTH_SERVER_ID
 global.OKTA_TENANT = process.env.OKTA_TENANT
+
 global.REDIRECT_URI_BASE = process.env.REDIRECT_URI_BASE
 
 global.SESSION_SECRET = process.env.SESSION_SECRET
